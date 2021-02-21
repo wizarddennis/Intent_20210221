0783df4299958a184b0cf2c11a86e4ad16b3cdea
dennis <travlInfo4you@gmail.com>
2021-02-21 12:46:13 +0900

    Intent 기초 활용

--- app/src/main/java/kr/co/tjoeun/intent_20210221/FirstActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20210221/FirstActivity.kt
@@ -1,11 +1,21 @@
 package kr.co.tjoeun.intent_20210221
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        moveToFirstActivity.setOnClickListener {
+            //firstActivity로 넘어가기
+            val myIntent = Intent(this, FirstActivity::class.java)
+
+            startActivity(myIntent)
+
+        }
     }
 }
